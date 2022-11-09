a201faa476996dfa61dc841dcc75c0e1e0b3461d
eiiizong <eiiizong@163.com>
2022-11-09 17:07:32 +0800

    commit: 框架搭建

--- src/store/getters/index.ts
+++ src/store/getters/index.ts
@@ -2,11 +2,11 @@ import { USER_INFO, USER_LIST } from '@/store/constants'
 
 const getters = {
   // 获取用户信息
-  [USER_INFO](state) {
+  USER_INFO(state) {
     return state.userInfo
   },
   // 获取用户信息
-  [USER_LIST](state) {
+  USER_LIST(state) {
     return state.userList
   },
 }
