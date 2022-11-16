b736eff7a473b9bd6287e56278dcd46f23f13138
[eiiizong] <[eiiizong@163.com]>
2022-11-16 22:39:42 +0800

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
-import { onLaunch, onShow, onHide } from "@dcloudio/uni-app";
-onLaunch(() => {
-  console.log("App Launch");
-});
-onShow(() => {
-  console.log("App Show");
-});
+import { onLaunch, onShow, onHide } from '@dcloudio/uni-app'
+
+onLaunch((e) => {
+  console.log('App Launch', e)
+})
+
+onShow((e) => {
+  console.log('App Show', e)
+})
+
 onHide(() => {
-  console.log("App Hide");
-});
+  console.log('App Hide')
+})
 </script>
 <style></style>
