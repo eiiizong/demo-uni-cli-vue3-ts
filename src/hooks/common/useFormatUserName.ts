1b0dfd023c854e70539f150293c2a1a3947fb076
[eiiizong] <[eiiizong@163.com]>
2022-11-21 19:46:17 +0800

    框架搭建

---
+++ src/hooks/common/useFormatUserName.ts
@@ -0,0 +1,23 @@
+/**
+ * 格式化姓名 张三 => *三 张三三 => 张*三
+ * @param {string} value 姓名
+ */
+const useFormatUserName = (value: string): string => {
+  let result = ''
+  value = value.trim()
+  if (value) {
+    const len = value.length
+
+    if (len === 2) {
+      result = value.substring(0, 1) + '*'
+    } else if (len === 3) {
+      result = value.substring(0, 1) + '*' + value.substring(2, 3)
+    } else if (len > 3) {
+      result = value.substring(0, 1) + '*' + '*' + value.substring(3, len)
+    }
+  }
+
+  return result
+}
+
+export { useFormatUserName }
