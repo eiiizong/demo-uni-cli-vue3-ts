19a68668873cd626cd467aca0bbd3c808512ba90
[eiiizong] <[eiiizong@163.com]>
2022-11-21 19:55:04 +0800

    框架搭建

--- src/hooks/common/useGetAgeByIDCard.ts
+++ src/hooks/common/useGetAgeByIDCard.ts
@@ -1,17 +1,17 @@
 /**
  * 根据身份证号获取年龄
- * @param {String} idCard 身份证号
+ * @param {String} value 身份证号码
  */
 
-const useGetAgeByIDCard = (idCard) => {
+const useGetAgeByIDCard = (value: string): number => {
   // 格式化身份证的值 去除空格 将 X 转化为 x
-  idCard = (idCard + '').trim().toUpperCase()
-  if (!idCard) {
+  value = (value + '').trim().toUpperCase()
+  if (!value) {
     console.error('身份证号码为空，请检查！')
     return 0
   }
 
-  const len = idCard.length
+  const len = value.length
 
   if (len != 15 && len != 18) {
     console.error('身份证号码只能为15位或18位,其它不合法，请检查！')
@@ -21,22 +21,11 @@ const useGetAgeByIDCard = (idCard) => {
   let birthday = ''
   // 处理15位的身份证号码从号码中得到生日和性别代码
   if (len == 15) {
-    birthday =
-      '19' +
-      idCard.substr(6, 2) +
-      '/' +
-      idCard.substr(8, 2) +
-      '/' +
-      idCard.substr(10, 2)
+    birthday = '19' + value.substring(6, 2) + '/' + value.substring(8, 2) + '/' + value.substring(10, 2)
   }
   // 处理18位的身份证号码从号码中得到生日和性别代码
   if (len == 18) {
-    birthday =
-      idCard.substr(6, 4) +
-      '/' +
-      idCard.substr(10, 2) +
-      '/' +
-      idCard.substr(12, 2)
+    birthday = value.substring(6, 4) + '/' + value.substring(10, 2) + '/' + value.substring(12, 2)
   }
 
   // 时间字符串里，必须是“/”
@@ -50,8 +39,7 @@ const useGetAgeByIDCard = (idCard) => {
   // 现在的月份等于生日月份并且现在的号数小于生日月份的号数 年龄减1
   if (
     nowDate.getMonth() < birthDate.getMonth() ||
-    (nowDate.getMonth() == birthDate.getMonth() &&
-      nowDate.getDate() < birthDate.getDate())
+    (nowDate.getMonth() == birthDate.getMonth() && nowDate.getDate() < birthDate.getDate())
   ) {
     age--
   }
--- src/hooks/project/index.js
+++
@@ -1,4 +0,0 @@
-export { useCheckPositionNameAuth } from './useCheckPositionNameAuth'
-export { useDeclareGetData } from './useDeclareGetData'
-export { useDeclareGetDataCodeTable } from './useDeclareGetDataCodeTable'
-// export { usePreviewPDFWeixin } from './usePreviewPDFWeixin'
