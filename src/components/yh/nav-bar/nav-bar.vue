882341f37e7a9d48b5aec151001ee373d7097058
eiiizong <eiiizong@163.com>
2022-12-03 14:58:54 +0800

    框架搭建

--- src/components/yh/nav-bar/nav-bar.vue
+++ src/components/yh/nav-bar/nav-bar.vue
@@ -1,5 +1,5 @@
 <template>
-  <view class="yh-nav-bar">
+  <view class="yh-nav-bar" :class="navBarClass" :style="navBarStyle">
     <view class="yh-nav-bar__left" @click="emit('click-left')">
       <block v-if="leftArrow || leftText">
         <yh-icon v-if="leftArrow" size="16px" name="arrow-left" custom-class="yh-nav-bar__arrow" />
@@ -9,14 +9,12 @@
       </block>
       <slot v-else name="left" />
     </view>
-    <view class="yh-nav-bar__title title-class yh-ellipsis">
+    <view class="yh-nav-bar__title">
       <view v-if="title">{{ title }}</view>
       <slot v-else name="title" />
     </view>
     <view class="yh-nav-bar__right" @click="emit('click-right')">
-      <view v-if="rightText" class="yh-nav-bar__text" hover-class="yh-nav-bar__text--hover" hover-stay-time="70">{{
-        rightText
-      }}</view>
+      <view v-if="rightText" class="yh-nav-bar__text">{{ rightText }}</view>
       <slot v-else name="right" />
     </view>
   </view>
@@ -24,33 +22,69 @@
 
 <script setup lang="ts">
 import type { Ref } from 'vue'
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, computed } from 'vue'
 
 const props = defineProps({
-  title: String,
-  fixed: Boolean,
+  title: {
+    type: String,
+    default: '',
+  },
+  fixed: {
+    type: Boolean,
+    default: true,
+  },
   // 左边文本
-  leftText: String,
+  leftText: {
+    type: String,
+    default: '',
+  },
   // 右边文本
-  rightText: String,
+  rightText: {
+    type: String,
+    default: '',
+  },
   // 左边返回箭头
-  leftArrow: Boolean,
+  leftArrow: {
+    type: Boolean,
+    default: true,
+  },
   border: {
     type: Boolean,
-    value: true,
+    default: true,
   },
   zIndex: {
     type: Number,
-    value: 1,
+    default: 1,
   },
   safeAreaInsetTop: {
     type: Boolean,
-    value: true,
+    default: true,
   },
 })
 
 const emit = defineEmits(['click-left', 'click-right'])
+
 const _statusBarHeight: Ref<number | undefined> = ref(0)
+
+const navBarStyle = computed(() => {
+  let str = ''
+  const { safeAreaInsetTop } = props
+  if (safeAreaInsetTop) {
+    str += `padding-top: ${_statusBarHeight.value}px`
+  }
+  return str
+})
+
+const navBarClass = computed(() => {
+  let str = ''
+  const { border } = props
+
+  if (border) {
+    str += `${border} `
+  }
+
+  return str
+})
 
 onMounted(() => {
   const { statusBarHeight } = uni.getSystemInfoSync()
@@ -61,7 +95,50 @@ onMounted(() => {
 <style lang="scss" scoped>
 .yh-nav-bar {
   width: 100%;
-  padding-top: var(--status-bar-height);
-  background-color: #f00;
+  position: relative;
+  text-align: center;
+  -webkit-user-select: none;
+  user-select: none;
+  line-height: 88rpx;
+  color: #fff;
+  background-color: $primary;
+  &.fixed {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+  }
+
+  &__title {
+    max-width: 50%;
+    margin: 0 auto;
+    color: inherit;
+    font-weight: 500;
+    font-size: 16px;
+    background-color: #f00;
+  }
+  &__left,
+  &__right {
+    position: absolute;
+    bottom: 0;
+    font-size: 14px;
+  }
+  &__left {
+    left: 16px;
+  }
+  &__right {
+    right: 16px;
+  }
+  &__text {
+    display: inline-block;
+    vertical-align: middle;
+    margin: 0 -16px;
+    padding: 0 16px;
+    color: inherit;
+    &:hover,
+    &--hover {
+      background-color: #f2f3f5;
+    }
+  }
 }
 </style>
--- src/pages/tabbar/home/home.vue
+++ src/pages/tabbar/home/home.vue
@@ -1,6 +1,6 @@
 <template>
   <view class="home">
-    <YhNavBar></YhNavBar>
+    <YhNavBar title="首页"></YhNavBar>
   </view>
 </template>
 
