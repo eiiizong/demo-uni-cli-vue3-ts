13a2d08fc8a064622245b516f7e21405ee232f85
eiiizong <eiiizong@163.com>
2023-01-03 21:50:43 +0800

    框架搭建

--- src/pages/packageProject/bills/BillFooter.vue
+++ src/pages/packageProject/bills/BillFooter.vue
@@ -1,13 +1,78 @@
 <template>
-  <view class="bill-total">bill-total</view>
+  <view class="bill-total">
+    <div class="item">
+      <img class="img" :src="imageBg01" alt="" />
+      <div class="content">
+        <div class="name">白名单企业</div>
+        <div class="value">
+          <div class="val">38,534</div>
+          <div class="unit">家</div>
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <img class="img" :src="imageBg02" alt="" />
+      <div class="content">
+        <div class="name">白名单获贷率</div>
+        <div class="value">
+          <div class="val">38%</div>
+        </div>
+      </div>
+    </div>
+  </view>
 </template>
 
 <script setup lang="ts">
+import imageBg01 from './images/bg-01.png'
+import imageBg02 from './images/bg-02.png'
+
 import { ref, reactive, onMounted } from 'vue'
 </script>
 
 <style lang="scss" scoped>
 .bill-total {
   width: 100%;
+  background-color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: $spacing;
+  border-top: 20rpx solid $color-page;
+  .item {
+    width: 330rpx;
+    height: 164rpx;
+    position: relative;
+    .img {
+      width: 100%;
+      height: 100%;
+    }
+    .content {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      padding: 40rpx 24rpx;
+      font-size: 28rpx;
+      color: #202429;
+      line-height: 40rpx;
+      .value {
+        display: flex;
+        align-items: center;
+        padding-top: 6rpx;
+        .val {
+          font-weight: 700;
+          font-size: 50rpx;
+          line-height: 58rpx;
+          color: #404040;
+        }
+        .unit {
+          font-size: 30rpx;
+          line-height: 1;
+          padding-left: 14rpx;
+        }
+      }
+    }
+  }
 }
 </style>
--- src/pages/packageProject/bills/BillTotal.vue
+++ src/pages/packageProject/bills/BillTotal.vue
@@ -1,13 +1,97 @@
 <template>
-  <view class="bill-total">bill-total</view>
+  <view class="bill-total">
+    <div class="total">
+      <div class="img-wrapper">
+        <img class="icon" :src="imageIcon01" alt="" />
+        <div class="text">正常</div>
+      </div>
+      <div class="info">
+        <div class="key">风险资金池</div>
+        <div class="value">
+          <div class="val">2424</div>
+          <div class="unit">万</div>
+        </div>
+      </div>
+    </div>
+    <div class="items">
+      <div class="item">
+        <div class="key">风险资金池</div>
+        <div class="value">
+          <div class="val">5555.04</div>
+          <div class="unit">万</div>
+        </div>
+      </div>
+      <div class="item">
+        <div class="key">风险资金池</div>
+        <div class="value">
+          <div class="val">5555.04</div>
+          <div class="unit">万</div>
+        </div>
+      </div>
+    </div>
+  </view>
 </template>
 
 <script setup lang="ts">
+import imageIcon01 from './images/icon-01.png'
 import { ref, reactive, onMounted } from 'vue'
 </script>
 
 <style lang="scss" scoped>
 .bill-total {
   width: 100%;
+  background-image: linear-gradient(to bottom, #dfefff 0%, #ffffff 100%);
+  margin-top: 20rpx;
+  padding-top: 22rpx;
+  .total {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    .img-wrapper {
+      position: relative;
+      .icon {
+        width: 184rpx;
+        height: 184rpx;
+      }
+      .text {
+        position: absolute;
+        left: 50%;
+        transform: translateX(-50%);
+        font-size: 28rpx;
+        line-height: 40rpx;
+        color: #ffffff;
+        bottom: 48rpx;
+      }
+    }
+
+    .info {
+      padding-left: 28rpx;
+      color: rgba(#000000, 0.5);
+      font-size: 36rpx;
+      line-height: 50rpx;
+      .value {
+        display: flex;
+        align-items: center;
+        padding-top: 20rpx;
+        color: #3965df;
+        .val {
+          font-size: 64rpx;
+          line-height: 72rpx;
+          font-weight: 700;
+        }
+        .unit {
+          font-size: 38rpx;
+          padding-left: 10rpx;
+        }
+      }
+    }
+  }
+  .items {
+    display: flex;
+    align-items: center;
+    .item {
+      width: 50%;
+    }
+  }
 }
 </style>
