7a8703b0609ee06392f3ea9d9f014d4f836b74af
eiiizong <eiiizong@163.com>
2023-01-29 18:16:34 +0800

    框架搭建

--- src/components/project/zdb-card-apply/zdb-card-apply.vue
+++ src/components/project/zdb-card-apply/zdb-card-apply.vue
@@ -1,30 +1,37 @@
 <template>
   <view class="zdb-card-apply" :class="space ? 'space' : ''">
-    <div class="title">
-      <div class="icon"></div>
-      <div class="name">担保费补贴申请</div>
-      <div class="status">进行中</div>
+    <div class="bg">
+      <img :ssrc="imageBg" alt="" class="img" />
     </div>
-    <div class="cells">
-      <div class="cell">
-        <div class="key">申报机构：</div>
-        <div class="value">担保融资公司</div>
+    <div class="con">
+      <div class="title">
+        <div class="icon"></div>
+        <div class="name">担保费补贴申请</div>
+        <div class="status">进行中</div>
       </div>
-      <div class="cell">
-        <div class="key">申报时间：</div>
-        <div class="value">担保融资公司</div>
+      <div class="cells">
+        <div class="cell">
+          <div class="key">申报机构：</div>
+          <div class="value">担保融资公司</div>
+        </div>
+        <div class="cell">
+          <div class="key">申报时间：</div>
+          <div class="value">担保融资公司</div>
+        </div>
       </div>
-    </div>
-    <div class="footer">
-      <div class="left">如需查看数据详情，请登录“蓉易贷”网站端查看</div>
-      <div class="right">
-        <span>流程一览</span>
+      <div class="footer">
+        <div class="left">如需查看数据详情，请登录“蓉易贷”网站端查看</div>
+        <div class="right">
+          <span>流程一览</span>
+        </div>
       </div>
     </div>
   </view>
 </template>
 
 <script setup lang="ts">
+import imageBg from './images/bg.png'
+
 import type { PropType } from 'vue'
 
 interface Item {
@@ -53,41 +60,5 @@ const props = defineProps({
 <style lang="scss" scoped>
 .zdb-table {
   width: 100%;
-  display: flex;
-  flex-wrap: wrap;
-  border: 1px solid $color-border;
-  &.space {
-    margin-top: 40rpx;
-  }
-  .col {
-    width: 50%;
-    font-size: 30rpx;
-    line-height: 42rpx;
-    .key {
-      height: 72rpx;
-      color: #999999;
-      background-color: #f2f2f2;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      border-bottom: 1px solid $color-border;
-    }
-    .value {
-      height: 72rpx;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      border-bottom: 1px solid $color-border;
-    }
-    &:nth-of-type(2n + 1) {
-      border-right: 1px solid $color-border;
-    }
-    &:nth-last-of-type(1),
-    &:nth-last-of-type(2) {
-      .value {
-        border-bottom: 0;
-      }
-    }
-  }
 }
 </style>
