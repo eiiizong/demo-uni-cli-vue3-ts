673a5c77ff2fa3f6f09ab99131caeb520c68e365
eiiizong <eiiizong@163.com>
2023-01-31 14:42:26 +0800

    框架搭建

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,88 +1,61 @@
 module.exports = {
-  root: true,
   env: {
     browser: true,
-    es2021: true,
-    node: true,
+    es2021: true
   },
   extends: [
     'plugin:vue/vue3-recommended',
-    'eslint:recommended',
-    '@vue/typescript/recommended',
-    '@vue/prettier',
-    '@vue/prettier/@typescript-eslint',
-    // eslint-config-prettier 的缩写
+    'plugin:@typescript-eslint/recommended',
     'prettier',
+    'plugin:prettier/recommended'
   ],
   parserOptions: {
-    ecmaVersion: 2021,
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
   },
-  plugins: [],
+  plugins: ['@typescript-eslint'],
   rules: {
-      // 首个参数 0 = off, 1 = warn, 2 = error
-      'prettier/prettier': 1,
-      'no-tabs': 'off',
-      'no-return-assign': 0,
-      'callback-return': 0,
-      'consistent-return': 0,
-      // js
-      'no-console': process.env.NODE_ENV === 'production' ? 1 : 0,
-      'no-debugger': process.env.NODE_ENV === 'production' ? 1 : 0,
-      // 禁止使用拖尾逗号
-      'comma-dangle': [2, 'never'],
-      // 禁止使用分号
-      'semi': [2, 'never'],
-      // 禁用行尾空白
-      'no-trailing-spaces': [2],
-      // 禁止直接调用 Object.prototypes 的内置属性
-      'no-prototype-builtins': 0,
-      // 强制一行的最大长度
-      'max-len': 0,
-      // 禁止对函数参数再赋值
-      'no-param-reassign': 0,
-      // 要求箭头函数的参数使用圆括号
-      'arrow-parens': 0,
-      // 禁用一元操作符 ++ 和 --
-      'no-plusplus': 0,
-      // 禁止使用特定的语法
-      'no-restricted-syntax': 0,
-  
-      // ts
-      // 禁用 any
-      '@typescript-eslint/no-explicit-any': 0,
-      // 禁用 空函数
-      '@typescript-eslint/no-empty-function': 0,
-      // 禁用 忽略
-      '@typescript-eslint/ban-ts-comment': 0,
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-  
-      // vue
-      // script 缩进
-      // 'vue/script-indent': [0, 2, { baseIndent: 1, switchCase: 1 }],
-      // prop 在 模板 和 JSX 中应该始终使用 kebab-case
-      'vue/attribute-hyphenation': [2, 'always'],
-      // 只有1个导出时，用export default
-      'import/prefer-default-export': 0,
-      'linebreak-style': ['off', 'windows'],
-      // 取消对文件扩展名的验证
-      'import/extensions': [0, 'always', {
-        js: '',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-        vue: 'never'
-      }],
-      // 强制在花括号内使用一致的换行符
-      'object-curly-newline': [0, { multiline: true }]
-  },
-  overrides: [
-    {
-      // 关闭 vue 文件中的 indent 的默认检查
-      // 使用 vue/script-indent 缩进规则
-      files: ['*.vue'],
-      rules: {
-        indent: 0
+    // 关闭vue文件和组件命名校验
+    'vue/multi-word-component-names': 0,
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
+    'vue/max-attributes-per-line': 0,
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'vue/no-v-html': 'off',
+    // 自动关闭标签定义
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always',
+          normal: 'never',
+          component: 'always'
+        },
+        svg: 'always',
+        math: 'always'
       }
-    }
-  ]
+    ],
+    // 在标记的右括号前换行
+    'vue/html-closing-bracket-newline': [
+      'error',
+      {
+        singleline: 'never',
+        multiline: 'never'
+      }
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'none'
+      }
+    ],
+    '@typescript-eslint/no-namespace': [
+      'error',
+      {
+        allowDeclarations: true
+      }
+    ]
+  }
 }
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,12 +1,15 @@
 module.exports = {
-  printWidth: 100,
-  tabWidth: 2,
-  tabs: false,
-  semi: false,
-  singleQuote: true,
-  quoteProps: 'as-needed',
-  bracketSpacing: true,
-  jsxBracketSameLine: false,
-  arrowParens: 'always',
-  endOfLine: 'auto',
+  printWidth: 120, // 代码单行长度
+  tabWidth: 2, // tab键缩进为2空格
+  useTabs: false, // 是否使用tabs缩进
+  singleQuote: true, // 用单引号
+  semi: false, // 句末是否加分号
+  bracketSameLine: true, // 标签换行后>单独一行
+  vueIndentScriptAndStyle: true,
+  trailingComma: 'none', // 最后一个对象元素不加逗号
+  bracketSpacing: true, // 对象，数组加空格
+  jsxBracketSameLine: true, // jsx > 是否另起一行
+  arrowParens: 'always', // (x) => {} 是否要有小括号
+  requirePragma: false, // 不需要写文件开头的 @prettier
+  insertPragma: false, // 不需要自动在文件开头插入 @prettier
 }
