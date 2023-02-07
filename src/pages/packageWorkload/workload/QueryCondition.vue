4b9f3a0f3b2cb572264a7a9b81f1810462c5bd4e
eiiizong <eiiizong@163.com>
2023-02-07 11:15:01 +0800

    框架搭建

--- src/pages/packageWorkload/workload/QueryCondition.vue
+++ src/pages/packageWorkload/workload/QueryCondition.vue
@@ -9,17 +9,17 @@
     <div class="input-wrapper">
       <div class="label">起止日期</div>
       <div class="value">
-        <picker class="picker">
+        <picker class="picker" mode="date">
           <view class="picker-content">
-            <div class="picker-value"></div>
+            <div class="picker-value">12</div>
             <div class="picker-placeholder"></div>
           </view>
         </picker>
-        <div>至</div>
-        <picker class="picker">
+        <div class="separate">至</div>
+        <picker class="picker" mode="date">
           <view class="picker-content">
             <div class="picker-value"></div>
-            <div class="picker-placeholder"></div>
+            <div class="picker-placeholder">请选择</div>
           </view>
         </picker>
       </div>
@@ -39,5 +39,46 @@
 <style lang="scss" scoped>
   .query-condition {
     width: 100%;
+    background-color: #fff;
+    padding: $spacing;
+    .input-wrapper {
+      display: flex;
+      align-items: center;
+      margin-bottom: $spacing;
+      font-size: 28rpx;
+      color: #404040;
+      .label {
+        margin-right: 16rpx;
+      }
+      .value {
+        flex: 1;
+        overflow: hidden;
+        display: flex;
+        align-items: center;
+        align-items: center;
+      }
+
+      .picker,
+      .input {
+        flex: 1;
+        height: 72rpx;
+        background-color: #fdfdfd;
+        border-radius: 4rpx;
+        border: solid 1px #999999;
+        font-size: inherit;
+        line-height: 68rpx;
+        padding: 0 26rpx;
+      }
+
+      .separate {
+        padding: 0 16rpx;
+      }
+      .picker-placeholder {
+        color: rgba(0, 0, 0, 0.3);
+      }
+      &:last-child {
+        margin-bottom: 0;
+      }
+    }
   }
 </style>
--- src/pages/packageWorkload/workload/QueryResult.vue
+++ src/pages/packageWorkload/workload/QueryResult.vue
@@ -1,83 +1,97 @@
 <template>
   <view class="query-result">
-    <div class="wrapper">
-      <ZdbTitle title="保费补贴支付" />
+    <div class="result-item">
       <div class="title-wrapper">
-        <div class="left">保费补贴支付</div>
-        <div class="right">共186笔</div>
+        <ZdbTitle title="保费补贴支付" />
       </div>
-      <div class="cells">
-        <div class="cell">
-          <div class="key">通过</div>
-          <div class="value">170笔</div>
+      <div class="content-wrapper">
+        <div class="name-wrapper">
+          <div class="left">保费补贴支付</div>
+          <div class="right">共186笔</div>
         </div>
-        <div class="cell">
-          <div class="key">退回</div>
-          <div class="value">170笔</div>
-        </div>
-        <div class="cell">
-          <div class="key">作废</div>
-          <div class="value">170笔</div>
+        <div class="cells">
+          <div class="cell">
+            <div class="key">通过</div>
+            <div class="value">170笔</div>
+          </div>
+          <div class="cell">
+            <div class="key">退回</div>
+            <div class="value">170笔</div>
+          </div>
+          <div class="cell">
+            <div class="key">作废</div>
+            <div class="value">170笔</div>
+          </div>
         </div>
       </div>
     </div>
-    <div class="wrapper">
-      <ZdbTitle title="合作机构准入" />
+    <div class="result-item">
       <div class="title-wrapper">
-        <div class="left">合作机构审核</div>
-        <div class="right">共186笔</div>
+        <ZdbTitle title="合作机构准入" />
       </div>
-      <div class="cells">
-        <div class="cell">
-          <div class="key">通过</div>
-          <div class="value">170笔</div>
+      <div class="content-wrapper">
+        <div class="name-wrapper">
+          <div class="left">合作机构审核</div>
+          <div class="right">共186笔</div>
         </div>
-        <div class="cell">
-          <div class="key">退回</div>
-          <div class="value">170笔</div>
-        </div>
-        <div class="cell">
-          <div class="key">作废</div>
-          <div class="value">170笔</div>
+        <div class="cells">
+          <div class="cell">
+            <div class="key">通过</div>
+            <div class="value">170笔</div>
+          </div>
+          <div class="cell">
+            <div class="key">退回</div>
+            <div class="value">170笔</div>
+          </div>
+          <div class="cell">
+            <div class="key">作废</div>
+            <div class="value">170笔</div>
+          </div>
         </div>
       </div>
     </div>
-    <div class="wrapper">
-      <ZdbTitle title="白名单企业" />
+    <div class="result-item">
       <div class="title-wrapper">
-        <div class="left">入库审核</div>
-        <div class="right">共186笔</div>
+        <ZdbTitle title="白名单企业" />
       </div>
-      <div class="cells">
-        <div class="cell">
-          <div class="key">通过</div>
-          <div class="value">170笔</div>
+      <div class="content-wrapper">
+        <div class="name-wrapper">
+          <div class="left">入库审核</div>
+          <div class="right">共186笔</div>
         </div>
-        <div class="cell">
-          <div class="key">退回</div>
-          <div class="value">170笔</div>
-        </div>
-        <div class="cell">
-          <div class="key">作废</div>
-          <div class="value">170笔</div>
+        <div class="cells">
+          <div class="cell">
+            <div class="key">通过</div>
+            <div class="value">170笔</div>
+          </div>
+          <div class="cell">
+            <div class="key">退回</div>
+            <div class="value">170笔</div>
+          </div>
+          <div class="cell">
+            <div class="key">作废</div>
+            <div class="value">170笔</div>
+          </div>
         </div>
       </div>
-      <div class="title-wrapper">
-        <div class="left">出库审核</div>
-        <div class="right">共186笔</div>
-      </div>
-      <div class="cells">
-        <div class="cell">
-          <div class="key">通过</div>
-          <div class="value">170笔</div>
+      <div class="content-wrapper">
+        <div class="name-wrapper">
+          <div class="left">出库审核</div>
+          <div class="right">共186笔</div>
         </div>
-        <div class="cell">
-          <div class="key">退回</div>
-          <div class="value">170笔</div>
-        </div>
-        <div class="cell">
-          <div class="key">作废</div>
-          <div class="value">170笔</div>
+        <div class="cells">
+          <div class="cell">
+            <div class="key">通过</div>
+            <div class="value">170笔</div>
+          </div>
+          <div class="cell">
+            <div class="key">退回</div>
+            <div class="value">170笔</div>
+          </div>
+          <div class="cell">
+            <div class="key">作废</div>
+            <div class="value">170笔</div>
+          </div>
         </div>
       </div>
     </div>
@@ -95,7 +109,79 @@
 </script>
 
 <style lang="scss" scoped>
+  $color1: #0071ff;
+  $color2: #ffa000;
+  $color3: #bfbfbf;
   .query-result {
     width: 100%;
+    .result-item {
+      .title-wrapper {
+        padding: 0 $spacing;
+      }
+      .content-wrapper {
+        background-color: #fff;
+        padding: 0 $spacing;
+        .name-wrapper {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          font-size: 28rpx;
+          line-height: 40rpx;
+          color: #666666;
+          padding: 24rpx 0 14rpx;
+          border-bottom: 1px solid #ddd;
+        }
+        .cells {
+          padding: 10rpx 0 24rpx;
+          .cell {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            color: $color1;
+            font-size: 28rpx;
+            line-height: 40rpx;
+            margin-bottom: 24rpx;
+            .key {
+              position: relative;
+              padding-left: 30rpx;
+              font-size: inherit;
+              color: inherit;
+              &::before {
+                content: '';
+                width: 16rpx;
+                height: 16rpx;
+                background-color: $color1;
+                border-radius: 50%;
+                position: absolute;
+                left: 0;
+                top: 50%;
+                transform: translateY(-50%);
+              }
+            }
+            .value {
+              font-size: inherit;
+              color: inherit;
+            }
+            &:last-child {
+              margin-bottom: 0;
+              color: $color3;
+              .key {
+                &::before {
+                  background-color: $color3;
+                }
+              }
+            }
+            &:nth-of-type(2) {
+              color: $color2;
+              .key {
+                &::before {
+                  background-color: $color2;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
   }
 </style>
